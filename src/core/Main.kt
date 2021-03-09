22a30e6f172697ff51d7ab5ee9d54e22a7cfe48e
Rassell <miraqq55@gmail.com>
2021-03-10 00:29:01 +0100

    Task 6

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
     print(findString("Kotlin"))
 }
 
-fun findString(isNullString: String): String {
+fun findString(isNullString: String?): String {
     if (isNullString != null && isNullString.length > 0) {
         return "String of length ${isNullString.length}"
     } else {
