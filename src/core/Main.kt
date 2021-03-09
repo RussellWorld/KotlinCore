856c4019732bfba588dfc507a83775352ff45d2c
Rassell <miraqq55@gmail.com>
2021-03-10 00:28:41 +0100

    Task 6

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,7 +1,13 @@
 package core
 
 fun main(args: Array<String>) {
-    print(sum(1, 4))
+    print(findString("Kotlin"))
 }
 
-fun sum(a: Int, b: Int, c: Int = 0, d: Int = 0) = a + b + c + d
+fun findString(isNullString: String): String {
+    if (isNullString != null && isNullString.length > 0) {
+        return "String of length ${isNullString.length}"
+    } else {
+        return "Empty or null string"
+    }
+}
