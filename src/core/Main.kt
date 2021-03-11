a02b1d7afecc69b76e0740c85ed6cd1940076e8f
Rassell <miraqq55@gmail.com>
2021-03-12 00:49:09 +0100

    Task 9

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,13 +1,8 @@
 package core
 
 fun main(args: Array<String>) {
-    val data1 = "aaab"
-    val data2 = "aaaaabb"
-    val regex = "a+b".toRegex()
-    val b1 = regex.matches(data1)
-    val b2 = regex.matches(data2)
-
-    println(b1)
-    println(b2)
+    for (sym in 'a'..'f') {
+        println(sym)
+    }
 }
 
