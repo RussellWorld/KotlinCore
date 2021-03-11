f40f1238a1b7583bee3c54fc5250102a96347b19
Rassell <miraqq55@gmail.com>
2021-03-12 00:55:13 +0100

    Task 10

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,8 +1,12 @@
 package core
 
 fun main(args: Array<String>) {
-    for (sym in 'a'..'f') {
-        println(sym)
-    }
+    println(isLatter('f'))
+    println(isLatter('1'))
+    println(checkNumber('5'))
 }
+
+fun isLatter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+fun checkNumber(c: Char) = c !in '0'..'9'
+
 
