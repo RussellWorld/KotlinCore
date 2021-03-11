77bc50e103efbd39b889631d98c20fcad756573b
Rassell <miraqq55@gmail.com>
2021-03-11 23:55:59 +0100

    Task 9 Регулярные выражения (Проверка совпадения всей строки)

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,22 +1,13 @@
 package core
 
 fun main(args: Array<String>) {
-    val arr = mutableListOf<Int>()
-    arr.addAll(listOf(5, -25, -55, 25, 636, 23, 3262, 236, 243, 23, -1, -6))
+    val data1 = "aaab"
+    val data2 = "aaaaabb"
+    val regex = "a+b".toRegex()
+    val b1 = regex.matches(data1)
+    val b2 = regex.matches(data2)
 
-    var temp = 0
-    for (i in 0..arr.count() - 2) {
-        for (j in 0..arr.count() - i - 2) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
-            }
-        }
-    }
-    arr.forEach { element ->
-        print("$element ")
-    }
-
+    println(b1)
+    println(b2)
 }
 
