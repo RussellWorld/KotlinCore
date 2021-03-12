a15142798d795041cc74f2faa5a0b3d1e89682d2
Rassell <miraqq55@gmail.com>
2021-03-12 01:03:25 +0100

    Task 11

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,12 +1,21 @@
 package core
 
 fun main(args: Array<String>) {
-    println(isLatter('f'))
-    println(isLatter('1'))
-    println(checkNumber('5'))
+    println(findMind(1, 3))
+    println(findMind(4, 2, true))
+    println(findMind(5, 3))
 }
 
-fun isLatter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
-fun checkNumber(c: Char) = c !in '0'..'9'
-
+fun findMind(n1: Int, n2: Int, absolute: Boolean = false): Int {
+    val v1: Int
+    val v2: Int
+    if (absolute) {
+        v1 = Math.abs(n1)
+        v2 = Math.abs(n2)
+    } else {
+        v1 = n1
+        v2 = n2
+    }
+    return if (v1 < v2) n1 else n2
+}
 
