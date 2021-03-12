4ade247912e0e5aa1708f229846de0e4beb4427c
Rassell <miraqq55@gmail.com>
2021-03-12 01:11:05 +0100

    Task 12

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,21 +1,11 @@
 package core
 
 fun main(args: Array<String>) {
-    println(findMind(1, 3))
-    println(findMind(4, 2, true))
-    println(findMind(5, 3))
+    printPrefix("one", "two", "three", "four", "five", prefix = "number: ")
 }
 
-fun findMind(n1: Int, n2: Int, absolute: Boolean = false): Int {
-    val v1: Int
-    val v2: Int
-    if (absolute) {
-        v1 = Math.abs(n1)
-        v2 = Math.abs(n2)
-    } else {
-        v1 = n1
-        v2 = n2
-    }
-    return if (v1 < v2) n1 else n2
+fun printPrefix(vararg words: String, prefix: String) {
+    for (m in words)
+        println(prefix + m)
 }
 
