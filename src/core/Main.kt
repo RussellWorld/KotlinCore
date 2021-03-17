02971acd5802b8aa872c8d8596ea33b99a0c7bf1
Rassell <miraqq55@gmail.com>
2021-03-17 23:54:37 +0100

    Task 16

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,77 +1,90 @@
 package core
+class SomeClass constructor(someValue: String) {
+    val value: String
 
-fun main(args: Array<String>) {
+    init {
+        value = someValue
+    }
+}
 
-    val arr = arrayOf(1, 2, 3, 4)
-    val arr2 = listOf(1, 2, 3).toIntArray()
-    val arr3 = arrayOfNulls<String>(5)
-    var i = 0
-    val arr4: Array<Int> = Array<Int>(4) { i++ }
-    arr4.forEach {
-        println(it)
+class SomeClass2 constructor(someValue: String) {
+    val value: String = someValue
+}
+
+class SomeClass3(someValue: String)
+
+class Simple
+
+open class Parent(someValue: String)
+
+class Child(someValue: String) : Parent(someValue)
+
+private class PrivateClass
+
+class Outer {
+    inner class Inner
+    class StaticInner
+}
+
+class Robot {
+
+    var x: Int = 0
+        set(value) {
+            if (value > field) println("moved to the right")
+            else if (value < field) println("moved to the left")
+            else println("stayed in his place")
+            field = value
+        }
+
+    var y: Int = 0
+        set(value) {
+            if (value > field) println("moved to the top")
+            else if (value < field) println("moved to the bottom")
+            else println("stayed in his place")
+            field = value
+        }
+
+    var isInTheCentre: Boolean = true
+        get() = x == 0 && y == 0
+        private set
+
+//    override fun toString(): String {
+//        return "${this.javaClass} [coordinates: x = $x, y = $y]"
+//    }
+
+//    override fun equals(other: Any?): Boolean {
+//        return other is Robot && x == other.x && y == other.y
+//    }
+
+    override fun hashCode(): Int {
+        return (x.hashCode() / 2) + (y.hashCode() / 2)
+    }
+}
+
+data class DataClass(val name: String, val id: Int)
+
+object Singleton {
+    const val serialNumber: Int = 654321984
+
+    fun logSerialNumber() = println("serial number = $serialNumber")
+}
+
+class Cat {
+    val name: String
+    var age: Int
+
+    private constructor(name: String, age: Int) {
+        this.name = name
+        this.age = age
     }
 
-    val intArr = intArrayOf(2, 6, 9)
-    val charArray = charArrayOf('q', 'r')
-    val boolArray = booleanArrayOf(true, false)
-
-
-    var nullableArray: Array<Int?>? = arrayOf(1, 5, null, 9)
-
-    nullableArray?.forEach {
-        println(it?.times(2))
+    override fun toString(): String {
+        return "${this.javaClass} [name = $name, age = $age]"
     }
 
-    nullableArray?.filterNotNull()
-
-    val mutableCollection: MutableCollection<String> = arrayListOf("")
-    val collection: Collection<String> = mutableCollection
-    println("collection size = ${collection.size}")
-    mutableCollection.add("qwe")
-    println("collection size = ${collection.size}")
-
-    val immutableCollection: Collection<String> = arrayListOf("")
-    val immutableCollectionPlatform: Collection<String> = JavaClass.getCollection(immutableCollection)//.add(null)
-
-    val set = setOf("qwe", "qwe", "asd")
-    println("${set.javaClass} size = ${set.size}")
-    val set2 = hashSetOf("qwe", "qwe", "asd")
-    val set3 = sortedSetOf("qwe", "qwe", "asd")
-
-    val list = listOf(1, 2, 3)
-
-    val listNotNull = listOfNotNull("q", "w", null, "p", "1", "2")
-    println(listNotNull)
-
-    var count = 0; while (count++ < 5) println(listNotNull.shuffled())
-
-    val map = mapOf("first" to 25, "second" to 63, "third" to 12)
-    println(map["second"])
-    println(map.get("first"))
-
-    val mutableMap = mutableMapOf("first" to 25, "second" to 63, "third" to 12)
-    mutableMap.put("fourth", 33)
-    println(mutableMap["fourth"])
-    mutableMap.put("first", 15)
-    println(mutableMap.get("first"))
-
-    val numbers = listOf("one", "two", "three", "four")
-    println(numbers.joinToString())
-    println(numbers.joinToString(prefix = "[", postfix = "]"))
-    println(numbers.joinToString(prefix = "{", postfix = "}", separator = "|"))
-}
-
-fun getCollection(collection: Collection<String>){
-    //collection.add()
-}
-
-fun getMutableCollection(collection: MutableCollection<String>){
-    collection.add("")
-}
-
-//свы можете быть уверены, что не измените коллекцию, из которой делаете копию
-fun copy(source: Collection<String>, target: MutableCollection<String>){
-    source.forEach {
-        target.add(it)
+    companion object {
+        fun getInstance(name: String, age: Int): Cat {
+            return Cat(name, age)
+        }
     }
 }
