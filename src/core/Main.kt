b6fdeffceb125ab302b5b1f31c0f2be6caae917f
Rassell <miraqq55@gmail.com>
2021-04-24 00:41:00 +0100

    Классы - Методы - Необязательные параметры

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,26 +1,21 @@
 package core
 
-class ArrayAssistant(var data: Array<Int>) {
-    fun getFirstLast(): Pair<Int, Int> {
-        var first = -1
-        var last = -1
-        val len = data.size
-        if (len > 0) {
-            first = data[0]
-            last = data[len - 1]
+class Greeting {
+    companion object {
+        fun sayGoodby(message: String = "Goodby!") {
+            println(message)
         }
-        return Pair(first, last)
+
+        fun sayHello(message: String = "Hello!") {
+            println(message)
+        }
     }
 }
 
 
 fun main() {
-    val ar = arrayOf(2, 3, 5)
-    val assistant = ArrayAssistant(ar)
-    val (first, last) = assistant.getFirstLast()
-    //first is 2
-    //last is 5
-
-    println("first = $first")
-    println("last = $last")
+    Greeting.sayGoodby()
+    //print "Goodby"
+    Greeting.sayHello("Hi")
+    //print "Hi"
 }
