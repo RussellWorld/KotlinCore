20419e14c5ed1d2981d58906b0027e0cf799a184
Rassell <miraqq55@gmail.com>
2021-04-24 00:55:09 +0100

    Классы - Методы-индексаторы - С одним параметром

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,16 +1,21 @@
 package core
 
-class Calc {
-    companion object {
-        fun getSum(n1: Int, n2: Int): Int {
-            return n1 + n2
-        }
+import kotlin.math.pow
+
+class PowerOfTwo {
+    operator fun get(i: Int): Int {
+        return 2.0.pow(i.toDouble()).toInt()
     }
 }
 
 
 fun main() {
-    val sum = Calc.getSum(5, 3)
-//sum is 8
-    println("sum = $sum")
+    val power2 = PowerOfTwo()
+    val p2 = power2[2]
+    //p2 is 4
+    val p8 = power2[8]
+    //p8 is 256
+    val p16 = power2[16]
+    //p16 is 65546
+    println("p2 = $p2, p8 = $p8, p16 = $p16")
 }
