aa9bab77d2e073e8f7b8d8e9a7cf1aef3e4dcd06
Rassell <miraqq55@gmail.com>
2021-04-23 23:29:41 +0100

    Классы - Конструкторы - Вторичный конструктор с параметрами по умолчанию

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,9 +1,26 @@
 package core
 
- class Man {
-   var name = ""
+class Man {
+    var name: String
+    var country: String
+
+    constructor(name: String = "unknown", country: String = "unknown") {
+        this.name = name
+        this.country = name
+    }
 }
 
+
 fun main() {
-    val man = Man()
+    val man1 = Man()
+    //man1.name is "unknown"
+    //man1.country is "unknown"
+
+    val man2 = Man("Vladimir")
+    //man2.name is "Vladimir"
+    //man2.country is "unknown"
+
+    val man3 = Man("Vladimir", "Brazil")
+    //man3.name is "Vladimir"
+    //man3.country is "Brazil"
 }
