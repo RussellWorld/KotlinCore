0cf1879fe256b9e7e05145768d78b1e3a4e9ccb5
Rassell <miraqq55@gmail.com>
2021-04-23 23:46:33 +0100

    Классы - Методы - Входные/выходные параметры

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,13 +1,18 @@
 package core
 
-class Greeting {
-    fun sayGoodluck() {
-        println("Goodluck!")
+class Swap {
+    fun strings(s1: Array<String>, s2: Array<String>) {
+        val temp = s2[0]
+        s2[0] = s1[0]
+        s1[0] = temp
     }
 }
 
 
 fun main() {
-    val greeting = Greeting()
-    greeting.sayGoodluck()
+    val s1 = arrayOf("A")
+    val s2 = arrayOf("B")
+    val swap = Swap()
+
+    //s1[0] is "B", s2[0] is "A"
 }
