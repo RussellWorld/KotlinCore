e210656e22ac9f0495b18e43645d977d6d9ea30b
Rassell <miraqq55@gmail.com>
2021-04-24 03:00:09 +0100

    Классы - Наследование - Переопределение метода

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,17 +2,19 @@ package core
 
 
 open class Shape {
-    open val lineCount: Int
-        get() = 0
+    open fun getArea(): Int {
+        return 0
+    }
 }
 
-open class Square : Shape() {
-    override val lineCount: Int
-        get() = 4
+class Square(var sideLength: Int) : Shape() {
+    override fun getArea(): Int {
+        return sideLength * sideLength
+    }
 }
 
 fun main() {
-    val square = Square()
-    //square.lineCount is 4
-
+    var square = Square(5)
+    var area = square.getArea()
+    //area is 25
 }
