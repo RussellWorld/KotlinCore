3d859e059c3bc3bcf766c6a0408782973b9ab43d
Rassell <miraqq55@gmail.com>
2021-04-24 23:16:05 +0100

    Классы - Проверка на равенство ссылок

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,10 +1,18 @@
 package core
 
-class Game {
-    //public field
-    var name = ""
-    var year = 0
+class Car(var Mark: String = "")
 
-    //private field
-    private var developer = ""
+fun main() {
+    val car1 = Car()
+    val car2 = Car()
+    val car3 = car1
+
+    val equal1 = car1 == car2
+    //equal1 is False
+
+    val equal2 = car1 == car3
+    //equal2 is True
+
+    println(equal1)
+    println(equal2)
 }
