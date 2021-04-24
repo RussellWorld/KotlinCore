a533313545711bd6cd637662b27f120733ef2c88
Rassell <miraqq55@gmail.com>
2021-04-24 23:09:35 +0100

    Классы - Определение и инициализация

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,28 +1,12 @@
 package core
 
+//Definition
+class SomeClass {
 
-abstract class Shape {
-    var color = ""
-
-    abstract fun fill(color: String)
-
-    fun draw() {
-        println(color)
-    }
 }
-
-class Square : Shape {
-    override fun fill(color: String) {
-        this.color = color
-    }
-}
-
 
 fun main() {
-    val shape = Shape() //<- Error
-
-    val square = Square()
-    square.fill("Red")
-    square.draw()
+    //Initialization
+    var someClass = SomeClass()
 
 }
