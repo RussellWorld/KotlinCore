47812659e9a49e46656463e5749027a21a244910
Rassell <miraqq55@gmail.com>
2021-04-25 00:13:42 +0100

    Классы - События

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,20 +1,48 @@
 package core
 
-import kotlin.math.sqrt
-
-class Square(var side: Double) {
-    var area: Double
-        get() = side * side
-        set(value) {
-            side = sqrt(value)
-        }
-}
-
 fun main() {
-    val square = Square(2.0)
-    //square.area is 4.0
-    println(square.area)
+    val statistic = Statistic()
+    val heroes = Game("Heroes")
+    val doom = Game("Doom")
 
-    square.area = 9.0
-    //square.side is 3.0
+    //subscribe to events
+    heroes.addListener(statistic)
+    doom.addListener(statistic)
+
+    doom.start()
+    heroes.start()
+    //statistic.lastGame is "Heroes"
+    //statistic.startsCount is 2
+
+    println(statistic.lastGame)
+    println(statistic.startsCount)
+
 }
+
+
+//In Kotlin there are no events type
+interface GameListener{
+    fun gameStarted(name: String)
+}
+
+class Game(private val name: String){
+    private val listeners = arrayListOf<GameListener>()
+    fun addListener(listener: GameListener){
+        listeners.add(listener)
+    }
+    fun start(){
+        for (listener in listeners) {
+            listener.gameStarted(name)
+        }
+    }
+}
+class Statistic: GameListener{
+    var startsCount = 0
+    var lastGame = ""
+
+    override fun gameStarted(name: String) {
+        startsCount++
+        lastGame = name
+    }
+}
+
