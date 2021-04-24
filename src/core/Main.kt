a8f9db5e883ea141ad25504982ed8401c8359b27
Rassell <miraqq55@gmail.com>
2021-04-25 00:19:05 +0100

    Классы - Создание копии

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,48 +1,14 @@
 package core
 
 fun main() {
-    val statistic = Statistic()
-    val heroes = Game("Heroes")
-    val doom = Game("Doom")
+    val square = Shape(4, "Square")
+    val squareCopy = square.copy()
 
-    //subscribe to events
-    heroes.addListener(statistic)
-    doom.addListener(statistic)
-
-    doom.start()
-    heroes.start()
-    //statistic.lastGame is "Heroes"
-    //statistic.startsCount is 2
-
-    println(statistic.lastGame)
-    println(statistic.startsCount)
-
+    println(squareCopy.name)
 }
 
-
-//In Kotlin there are no events type
-interface GameListener{
-    fun gameStarted(name: String)
-}
-
-class Game(private val name: String){
-    private val listeners = arrayListOf<GameListener>()
-    fun addListener(listener: GameListener){
-        listeners.add(listener)
-    }
-    fun start(){
-        for (listener in listeners) {
-            listener.gameStarted(name)
-        }
+class Shape(val lineCount: Int, val name: String) {
+    fun copy(): Shape {
+        return Shape(lineCount, name)
     }
 }
-class Statistic: GameListener{
-    var startsCount = 0
-    var lastGame = ""
-
-    override fun gameStarted(name: String) {
-        startsCount++
-        lastGame = name
-    }
-}
-
