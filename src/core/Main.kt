24e5dc512cddb918a80a5ae1cc07742c2ba91869
Rassell <miraqq55@gmail.com>
2021-04-25 20:54:47 +0100

    Структуры (записи) - Определение и инициализация

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,9 +1,12 @@
 package core
 
 fun main() {
+    val p1 = ColorPoint(0, 0)
+    val x1 = p1.x
+    //x1 is 0
 
+    val p2: ColorPoint
+    val x2 = p2.x // <- Error
 }
 
-class ColorPoint(var x: Int, var y: Int) {
-    val color = "no color"
-}
+class ColorPoint(var x: Int, var y: Int)
