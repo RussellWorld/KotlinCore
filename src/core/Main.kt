6b66e22eaff72c0a1b126ad4e8e4e7b70688b484
Rassell <miraqq55@gmail.com>
2021-04-25 23:47:58 +0200

    Расшерения - Добавление свойств

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,22 +1,16 @@
 package core
 
 fun main() {
-    val rect = Rect(Point(3, 4), Size(10, 12))
-    val area = rect.getArea()
-    //area is 120
+
+    val size = Size(4, 5)
+    val area = size.area
+    //size is 20
+
     println(area)
 }
 
-class Point(var x: Int, var y: Int) {}
+class Size(var wight: Int, var height: Int)
 
-class Size(var width: Int, var heigth: Int) {}
+val Size.area: Int
+    get() = this.height * this.wight
 
-class Rect(val point: Point, var size: Size) {
-    fun Size.area(): Int {
-        return this.heigth * this.width
-    }
-
-    fun getArea(): Int {
-        return size.area()
-    }
-}
