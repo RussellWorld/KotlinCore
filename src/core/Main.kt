b7f72e4062b95185eee56836938816b95b00c051
Rassell <miraqq55@gmail.com>
2021-04-25 23:42:44 +0200

    Расшерения - Расшерение, как член типа

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,9 +1,22 @@
 package core
 
 fun main() {
-    add3AndPrint(5)
+    val rect = Rect(Point(3, 4), Size(10, 12))
+    val area = rect.getArea()
+    //area is 120
+    println(area)
 }
 
-fun add3AndPrint(value: Int) {
-    println(value + 3)
+class Point(var x: Int, var y: Int) {}
+
+class Size(var width: Int, var heigth: Int) {}
+
+class Rect(val point: Point, var size: Size) {
+    fun Size.area(): Int {
+        return this.heigth * this.width
+    }
+
+    fun getArea(): Int {
+        return size.area()
+    }
 }
