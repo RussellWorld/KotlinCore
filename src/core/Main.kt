9c888d6092e69aeadded3e9a948c26066c2ac91e
Rassell <miraqq55@gmail.com>
2021-04-26 00:01:06 +0200

    Расшерения - Добавление методов типа

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,16 +1,24 @@
 package core
 
+import java.lang.Math.pow
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 fun main() {
+    val p1 = Point(1.0, 3.0)
+    val p2 = Point(2.0, 4.0)
+    val distance = Point.distanceTo(p1, p2)
+    //distance is 1.4142
+    println(distance)
 
-    val size = Size(4, 5)
-    val area = size.area
-    //size is 20
-
-    println(area)
 }
 
-class Size(var wight: Int, var height: Int)
+class Point(var x: Double, var y: Double) {
+    companion object {}
+}
 
-val Size.area: Int
-    get() = this.height * this.wight
-
+fun Point.Companion.distanceTo(p1: Point, p2: Point): Double {
+    val d1 = pow(p1.x - p2.x, 2.0)
+    val d2 = (p1.y - p2.y).pow(2.0)
+    return sqrt(d1 + d2)
+}
