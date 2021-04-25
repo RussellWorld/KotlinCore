afa7f62e2662f08e1e4b79db26e39a8879decf08
Rassell <miraqq55@gmail.com>
2021-04-26 01:36:09 +0200

    Работа с простыми типами - Числа - Integer - Получение случайных значений

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,31 +1,9 @@
 package core
 
-import java.text.DecimalFormat
-
 
 fun main() {
-    val exp = 2.798798797
-
-    val s1 = exp.toString()
-    println(s1)
-    //s1 is 2.798798797
-
-    val s2 = "%.3f".format(exp)
-    println(s2)
-    //s2 is 2.799
-
-    val s3 = "%2.2e".format(exp / 100)
-    println(s3)
-    //s3 is 2.80e-02
-
-    val df = DecimalFormat("#,###.00")
-    var symbols = df.decimalFormatSymbols
-    symbols.groupingSeparator = ' '
-    df.decimalFormatSymbols = symbols
-    val s4 = df.format(exp * 1_000_000)
-    println(s4)
-    //s4 is 2 798 798.797
-
-
+    var value = (0..2).random()
+    println(value)
+    //value is 0, 1 or 2
 }
 
