1f2ed26a27150cc15e2bc5b871281b6653824b9c
Rassell <miraqq55@gmail.com>
2021-04-26 01:15:08 +0200

    Работа с простыми типами - Числа - Double and Float - Конвертация из строки

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,21 +1,22 @@
 package core
 
-import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.round
 
 fun main() {
-    val pi = 3.1415
-    var piRound = round(pi * 1000) / 1000.0
-    //piRound is 3.142
+    //the first method
+    val strPi = "3.14"
+    val pi = strPi.toFloat()
+    println(pi)
 
-    var piTrunc = floor(pi * 1000) / 1000
-    //piTrunc is 3.141
+    //the second method
+    val strExp = "2.71828"
+    val exp = strExp.toFloatOrNull()
+    if (exp != null) {
+        println(exp)
+    }
 
-    var piCeil = ceil(pi * 100) / 100
-    //piCell is 3.15
-
-    println(piCeil)
-
+    //the third method
+    val strHalf = "0,5"
+    val half = strHalf.replace(",", ".").toDouble()
+    println(half)
 }
 
