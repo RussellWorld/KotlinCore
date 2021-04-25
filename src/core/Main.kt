370bc26731e5a2060d49f0a24ada24ae5d0e6f59
Rassell <miraqq55@gmail.com>
2021-04-25 22:38:29 +0200

    Функции - Множество возвращаемых значений

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,12 +1,21 @@
 package core
 
 fun main() {
-    val sum = getSum(5, 3)
-    //sum is 8
-    println("sum = $sum")
+    val ar = arrayOf(2, 3, 6, 7)
+    val (first, last) = getFirstLast(ar)
+    //first is 2
+    //last is 7
+    println("first = $first, last = $last")
 }
 
-fun getSum(n1: Int, n2: Int): Int {
-    return n1 + n2
+fun getFirstLast(ar: Array<Int>): Pair<Int, Int> {
+    var first = -1
+    var last = -1
+
+    if (ar.isNotEmpty()) {   //ar.size > 0
+        first = ar[0]
+        last = ar[ar.size - 1]
+    }
+    return Pair(first, last)
 }
 
