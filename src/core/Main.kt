90630f6b61e1cb8a38f04e5f607da103d7692882
Rassell <miraqq55@gmail.com>
2021-04-25 23:13:48 +0200

    Функции - Без параметров

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,17 +1,9 @@
 package core
 
 fun main() {
-    val s1 = arrayOf("A")
-    val s2 = arrayOf("B")
-    swapStrings(s1, s2)
-    //s1[0] is "B", s2[0] is "A"
-    println("s1[0] = ${s1[0]}")
-    println("s2[0] = ${s2[0]}")
+    sayGoodby()
 }
 
-fun swapStrings(s1: Array<String>, s2: Array<String>) {
-    val temp = s2[0]
-    s2[0] = s1[0]
-    s1[0] = temp
-
+fun sayGoodby() {
+    println("Goodby!")
 }
