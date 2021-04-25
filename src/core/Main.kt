37f0f13e0ce281f6fac6424e82b64e29ed226a73
Rassell <miraqq55@gmail.com>
2021-04-25 20:51:48 +0100

    Структуры (записи) - Поля и свойства

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,19 +1,9 @@
 package core
 
 fun main() {
-    Setting.mode = 3
-    Setting.setNextMode()
+
 }
 
-class Setting {
-    companion object {
-        //type property
-        var path = ""
-        var mode: Int = 0
-
-        //type method
-        fun setNextMode() {
-            mode = (mode + 1) % 3
-        }
-    }
+class ColorPoint(var x: Int, var y: Int) {
+    val color = "no color"
 }
