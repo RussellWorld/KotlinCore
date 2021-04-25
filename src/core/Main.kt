320c4281581f13ef184ef85a116a901ab9cb76b8
Rassell <miraqq55@gmail.com>
2021-04-26 01:02:29 +0200

    Работа с простыми типами - Числа - Double and Float - Получение случайных значений

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,22 +1,11 @@
 package core
 
-import java.lang.Math.pow
-import kotlin.math.pow
-import kotlin.math.sqrt
+import java.util.*
 
 fun main() {
-    val p1 = Point(1.0, 3.0)
-    val p2 = Point(2.0, 4.0)
-    val distance = p1.distanceTo(p2)
-    //distance is 1.4142
-    println(distance)
+    val value = Random().nextDouble()
+    println(value)
+    //value double between 0.0 and 1.0
 
 }
 
-class Point(var x: Double, var y: Double)
-
-fun Point.distanceTo(p: Point): Double {
-    val d1 = pow(this.x - p.x, 2.0)
-    val d2 = (this.y - p.y).pow(2.0)
-    return sqrt(d1 + d2)
-}
