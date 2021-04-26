fd5f52860bbda5ac30650931e2286cd89e779fce
Rassell <miraqq55@gmail.com>
2021-04-26 17:52:10 +0200

    Поток управления - Циклы - цикл While

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,9 +1,14 @@
 package core
 
 fun main() {
-    val sunIsStar = true
-    val str = sunIsStar.toString()
-    println(str)
-    //str is "True"
+    var i = 5
+    var f5 = 1
+
+    while (i > 1) {
+        f5 *= i
+        i--
+    }
+    //f5 is 120
+    println(f5)
 }
 
