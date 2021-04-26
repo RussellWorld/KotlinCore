01fe95bb920b504d5884dea755140dab6b28bab6
Rassell <miraqq55@gmail.com>
2021-04-26 20:48:00 +0200

    Перегрузка оператора - Операторы сравнения

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,22 +2,21 @@ package core
 
 fun main() {
     val p1 = Point(1, 2)
-    val p2 = Point(2, 2)
-    val p3 = Point(1, 1)
-    val equal1 = p1 == p2
-    //equal is False
-    val equal2 = p1 == p3
-    //equal2 is False
-    val equal3 = p1 != p3
-    //equal3 is True
-    println(equal1)
-    println(equal2)
-    println(equal3)
+    val p2 = Point(2, 3)
+
+    val b1 = p1 > p2
+    //b1 is False
+    val b2 = p1 < p2
+    //b2 is True
 
 }
 
 class Point(var x: Int, var y: Int) {
-    override fun equals(other: Any?): Boolean {
-        return (other is Point) && (x == other.x) && (y == other.y)
+    operator fun compareTo(p: Point): Int {
+        if (x > p.x && y > p.y)
+            return 1
+        else if (x == p.x && y == p.y)
+            return 0
+        else return -1
     }
 }
