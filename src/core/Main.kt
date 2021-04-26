858195890c1d1c9e775ebcf88f542845d04c5758
Rassell <miraqq55@gmail.com>
2021-04-26 04:15:02 +0200

    Работа с простыми типами - Дата и время - получение текущей даты

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,16 +1,8 @@
 package core
 
-fun main() {
-    //There are no Typle type in Kotlin
-    val one = Pair(1, "one")
-    val number1 = one.component1()
-    //number is 1
-    val str1 = one.component2()
-    //str is "one"
+import java.time.LocalDateTime
 
-    val two = Pair<Int, String>(2, "two")
-    val (number2, str2) = two
-    //number is 2
-    //str is "two"
+fun main() {
+  val now = LocalDateTime.now()
 }
 
