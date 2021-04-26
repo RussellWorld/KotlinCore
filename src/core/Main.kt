75c3c333a1090d47d07300addb52136165b55cf0
Rassell <miraqq55@gmail.com>
2021-04-27 00:48:35 +0200

    Инициализация типов - Колекции - Множество

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,26 +2,8 @@ package core
 
 
 fun main() {
-    var languages = hashMapOf(
-            "ru" to "russian",
-            "en" to "english"
-    )
+    var intHashSet = setOf(2, 3, 5, 7, 11, 13, 17, 19)
 
-    var numbers = hashMapOf(
-            1 to "one",
-            2 to "two",
-            3 to "three"
-    )
-
-    var employees = hashMapOf(
-            1 to Employee("Pavlov", "Anton"),
-            2 to Employee("Pavlov", "Anton")
-    )
-
-    println(languages["ru"])
-    println(numbers[1])
-    println(employees[1]?.FirstName)
+    println(intHashSet)
 }
-
-class Employee(val FirstName: String, val LastName: String)
 
