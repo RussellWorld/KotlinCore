efef33fccbe7800e5a29022b0685876e0633a04e
Rassell <miraqq55@gmail.com>
2021-04-26 04:08:15 +0200

    Работа с простыми типами - Кортеж

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,18 +1,16 @@
 package core
 
-
 fun main() {
-    val str = "ABC"
-    val charA = str[0]
+    //There are no Typle type in Kotlin
+    val one = Pair(1, "one")
+    val number1 = one.component1()
+    //number is 1
+    val str1 = one.component2()
+    //str is "one"
 
-    val charB = str[1]
-
-    val charC = str[2]
-
-    var charList = ""
-    for (c in str){
-        charList += c + "; "
-    }
-    println(charList)
+    val two = Pair<Int, String>(2, "two")
+    val (number2, str2) = two
+    //number is 2
+    //str is "two"
 }
 
