a12b3937c604c83cfc26b045772fc8a35c075a09
Rassell <miraqq55@gmail.com>
2021-04-26 03:51:04 +0200

    Работа с простыми типами - Строки - Замена подстроки

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,11 +2,19 @@ package core
 
 
 fun main() {
-    var str = "1-3-2"
-    var arStr = str.split("").toTypedArray()
-    arStr[3] = "2"
-    arStr[5] = "3"
-    str = arStr.joinToString("")
-    println(str)
+    var startString = "3, 2, 1, go!"
+    startString = startString
+            .replace("1", "one")
+            .replace("2", "two")
+            .replace("3", "three")
+    //startString = "three, two, one, go!"
+
+    //replace all
+    var oneString = "1 1 1"
+    oneString = oneString
+            .replace("1", "one")
+
+    println("$startString   $oneString")
+    //oneString is "one one one"
 }
 
