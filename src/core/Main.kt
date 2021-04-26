d89464f984fbacd489f6d7b944a17163d9a0e575
Rassell <miraqq55@gmail.com>
2021-04-26 17:39:56 +0200

    Работа с простыми типами - Boolean - Конвертация из строки

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,12 +1,8 @@
 package core
 
 fun main() {
-    val name = "Alex"
-    val nameExists = name.isNotEmpty()
-    //name exists is True
-
-    val number = 7
-    val isTen = number == 10
-    //isTen is False
+    val str = "true"
+    val boolValue = str.toBoolean()
+    println(boolValue)
 }
 
