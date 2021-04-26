0fe2615e57395f4f7a5ab3ec1cb7184562d2e726
Rassell <miraqq55@gmail.com>
2021-04-27 01:41:30 +0200

    Массивы и коллекции - Словари - Проверка наличия ключа

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -4,9 +4,12 @@ fun main() {
     val dic = mapOf(
             1 to "one", 2 to null
     )
-    val upperDic = dic
-            .mapValues { it.value?.toUpperCase() }
+    val exists1 = dic.containsKey(1) //true
+    val exists2 = dic.containsKey(2) //true
+    val exists3 = dic.containsKey(3) //false
 
-    println(upperDic)
+    println(exists1)
+    println(exists2)
+    println(exists3)
 }
 
