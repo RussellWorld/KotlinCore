2d364e0b2dfbeeffbc3ecf6363b808fc5a775568
Rassell <miraqq55@gmail.com>
2021-04-26 20:52:33 +0200

    Перегрузка оператора - Бинарные операторы

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,22 +1,18 @@
 package core
 
 fun main() {
-    val p1 = Point(1, 2)
-    val p2 = Point(2, 3)
-
-    val b1 = p1 > p2
-    //b1 is False
-    val b2 = p1 < p2
-    //b2 is True
-
+    val p1 = Point(1, 1)
+    val p2 = Point(2, 2)
+    var p3 = p1 + p2
+    //p3.x is 3 and p3.y is 3
+    println(p3)
+    p3 += Point(3, 5)
+    //p3.x is 6 and p3.y is 8
+    println(p3)
 }
 
-class Point(var x: Int, var y: Int) {
-    operator fun compareTo(p: Point): Int {
-        if (x > p.x && y > p.y)
-            return 1
-        else if (x == p.x && y == p.y)
-            return 0
-        else return -1
+data class Point(var x: Int, var y: Int) {
+    operator fun plus(p: Point): Point {
+        return Point(x + p.x, y + p.y)
     }
 }
