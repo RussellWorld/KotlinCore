92601a8b28725f4906644655b0af5600833dfcf1
Rassell <miraqq55@gmail.com>
2021-04-26 02:56:01 +0200

    Работа с простыми типами - Строки - Поиск подстроки

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,8 +2,16 @@ package core
 
 
 fun main() {
-    val str = "one way ticket"
-    val way = str.substring(4..7)
-    //way is "way"
+    val dataString = "Substring search"
+
+    if (dataString.contains("string")) {
+        println("contain \"string\"")
+    }
+    if (dataString.startsWith("Sub")) {
+        println("starts with \"Sub\"")
+    }
+    if (dataString.endsWith("search")) {
+        println("ends with \"search\"")
+    }
 }
 
