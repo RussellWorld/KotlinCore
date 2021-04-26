c620c10bd702042509f9d30e0bd1d66cc6a74344
Rassell <miraqq55@gmail.com>
2021-04-26 03:55:06 +0200

    Работа с простыми типами - Строки - Замена нескольких символов

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,19 +2,12 @@ package core
 
 
 fun main() {
-    var startString = "3, 2, 1, go!"
-    startString = startString
-            .replace("1", "one")
-            .replace("2", "two")
-            .replace("3", "three")
-    //startString = "three, two, one, go!"
-
-    //replace all
-    var oneString = "1 1 1"
-    oneString = oneString
-            .replace("1", "one")
-
-    println("$startString   $oneString")
-    //oneString is "one one one"
+    var str = "1-/[=2=/]-3"
+    val separators = "[=/\\[]|]"
+    str = str.replace(
+            separators.toRegex(), ""
+    )
+    //str is 1-2-3
+    println(str)
 }
 
