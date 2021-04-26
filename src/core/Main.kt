f6b5ac6925e4d771fff6d9f2b28f996417e8650e
Rassell <miraqq55@gmail.com>
2021-04-26 04:52:25 +0200

    Работа с простыми типами - Boolean - получение значения

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,31 +1,12 @@
 package core
 
-import java.time.LocalDateTime
-import java.util.*
-
-
 fun main() {
-    //example with LocalDate
-    val now = LocalDateTime.now()
-    val yesterday = now.minusDays(1)
-    val tomorrow = now.plusDays(1)
-    val nextMonth = now.plusMonths(1)
-    val nextYear = now.plusYears(1)
+    val name = "Alex"
+    val nameExists = name.isNotEmpty()
+    //name exists is True
 
-    //example with Date
-    val cal = Calendar.getInstance()
-    cal.add(Calendar.DAY_OF_YEAR, -1)
-    val yeasterday1 = cal.time
-
-    cal.add(Calendar.DATE, 1)
-    val tomorrow1 = cal.time
-
-    cal.add(Calendar.MONTH, 1)
-    val nextMonth1 = cal.time
-
-    cal.add(Calendar.YEAR, 1)
-    val nextYear1 = cal.time
-
-
+    val number = 7
+    val isTen = number == 10
+    //isTen is False
 }
 
