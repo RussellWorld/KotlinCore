9c74e2fe1cde5db0226edc92af5c0ab58d2a1e53
Rassell <miraqq55@gmail.com>
2021-04-26 02:44:06 +0200

    Работа с простыми типами - Строки -Пустые строки

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,19 +2,11 @@ package core
 
 
 fun main() {
-    val strData = "1981|Kim Victoriya|engineer"
-    val arrData = strData.split("|")
+    val someEmptyString = ""
 
-    val year = arrData[0].toInt()
-    //year is 1981
-
-    val name = arrData[1]
-    //name is Kim Victoriya
-
-    val position = arrData[2]
-    //position is engeneer
-
-    println("$year $name $position")
+    if (someEmptyString.isEmpty()) {
+        println("string is empty")
+    }
 
 }
 
