c9e154d99ee6cca269564b62577a9c775734dc8b
Rassell <miraqq55@gmail.com>
2021-04-26 19:09:52 +0200

    Поток управления - Прерывание потока управлеия - Оператор return - Без возвращаемого значения

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,20 +1,22 @@
 package core
 
 fun main() {
-    val numbers = arrayOf(1, 2, 3)
-    val isContain2 = ContaintNumber(numbers, 2)
-    //isContain2 is true
-
-    val isContain4 = ContaintNumber(numbers, 4)
-    //isContain4 is false
 
 }
 
-fun ContaintNumber(numbers: Array<Int>, number: Int): Boolean {
-    for (currentNumber in numbers) {
-        if (currentNumber == number)
-            return true
+fun PrintSomeData(printAll: Boolean) {
+    PrintMainData()
+    if (!printAll) {
+        return
     }
-    return false
+    PrintOtherData()
+}
+
+fun PrintOtherData() {
+    TODO("Not yet implemented")
+}
+
+fun PrintMainData() {
+    TODO("Not yet implemented")
 }
 
