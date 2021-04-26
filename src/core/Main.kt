fe66573698429e2c777e1206d782efbb1b31025a
Rassell <miraqq55@gmail.com>
2021-04-26 19:23:15 +0200

    Поток управления - Прерывание потока управлеия - Именнованные метки @

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,18 +1,17 @@
 package core
 
 fun main() {
-    val numbers = arrayOf(2, 3, 5, 7, 11, 13, 17, 19)
-    var str = ""
-    for (i in numbers) {
-        if (i > 10) {
-            break
+    var firstMatchValue = -1
+    val array1 = arrayOf(1, 2, 3)
+    val array2 = arrayOf(2, 3, 4)
+
+    firstLoop@ for (value1 in array1) {
+        for (value2 in array2) {
+            if (value1 == value2){
+                firstMatchValue = value2
+                break@firstLoop
+            }
         }
-        if (str.isNotEmpty()) {
-            str += "-"
-        }
-        str += i
     }
-    //str is "2-5-11-17"
-    println(str)
 }
 
