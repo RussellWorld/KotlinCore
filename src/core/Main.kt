ed6340aa5cf05410e165f6504ec1d0065884e335
Rassell <miraqq55@gmail.com>
2021-04-27 01:25:03 +0200

    Массивы и коллекции - Словари - Обход словаря

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -4,8 +4,14 @@ fun main() {
     val dic = mapOf(
             1 to "one", 2 to "two"
     )
-    var count = dic.count()
-
-    println(count)
+    var str = ""
+    for ((k, v) in dic) {
+        if (str.isNotEmpty()) {
+            str += ", "
+        }
+        str += "{$k, \"$v\" }"
+        //str is { 1, "One"} , {2, "two" }
+    }
+    println(str)
 }
 
