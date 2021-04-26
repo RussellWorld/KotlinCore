d37890a35ccca4a228bb648527bb8ecae42b0c27
Rassell <miraqq55@gmail.com>
2021-04-26 03:28:52 +0200

    Работа с простыми типами - Строки - Объединение списка строк

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,16 +2,8 @@ package core
 
 
 fun main() {
-    val dataString = "Substring search"
-
-    if (dataString.contains("string")) {
-        println("contain \"string\"")
-    }
-    if (dataString.startsWith("Sub")) {
-        println("starts with \"Sub\"")
-    }
-    if (dataString.endsWith("search")) {
-        println("ends with \"search\"")
-    }
+    val numbers = arrayOf("one", "two", "three")
+    val numberList = numbers.joinToString { "; " }
+    //nuberList is "one; two; three"
 }
 
