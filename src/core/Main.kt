fb5c430ee43caa7c544cef0e473a0c73a61c8635
Rassell <miraqq55@gmail.com>
2021-04-26 04:43:57 +0200

    Работа с простыми типами - Дата и время - инициализация даты

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,26 +1,14 @@
 package core
 
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 
 fun main() {
     val cal = Calendar.getInstance()
-    cal.set(1945, 4, 9, 0, 0, 0)
+    cal.set(1945, 5, 9, 0, 0, 0)
     val victoryDate = cal.time
-    val now = Date()
 
-    val days = getDateDiff(victoryDate, now, TimeUnit.DAYS)
-    //days is 27746
-    val minutes = getDateDiff(victoryDate, now, TimeUnit.MINUTES)
-    //minutes is 37_054_603
-
-    println(days)
-    println(minutes)
+    println(victoryDate)
 
 }
 
-fun getDateDiff(date1: Date, date2: Date, timeUnit: TimeUnit): Long {
-    val diffInMillies = date2.time - date1.time
-    return timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS)
-}
