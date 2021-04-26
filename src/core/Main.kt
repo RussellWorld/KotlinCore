b407c2a7a50ed9597a5406b249f6555fe6821eb7
Rassell <miraqq55@gmail.com>
2021-04-26 21:05:24 +0200

    Обработка исключений - Генерация исключения

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,18 +1,24 @@
 package core
 
+import java.lang.Exception
+
 fun main() {
-    val p1 = Point(1, 1)
-    val p2 = Point(2, 2)
-    var p3 = p1 + p2
-    //p3.x is 3 and p3.y is 3
-    println(p3)
-    p3 += Point(3, 5)
-    //p3.x is 6 and p3.y is 8
-    println(p3)
+    val seller = Seller()
+    try {
+        seller.sell() // <- Error
+    } catch (e: Exception) {
+        println(e.message)
+        //e.message is "no car for sale
+    }
 }
 
-data class Point(var x: Int, var y: Int) {
-    operator fun plus(p: Point): Point {
-        return Point(x + p.x, y + p.y)
+class Car {}
+
+class Seller {
+    var cars = arrayOf<Car>()
+    fun sell() {
+        if (!cars.any()) {
+            throw Exception("No cars for sale")
+        }
     }
 }
