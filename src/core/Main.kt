c515760b4442a1e7847bffbf5f1c886417f6ff01
Rassell <miraqq55@gmail.com>
2021-04-27 01:33:55 +0200

    Массивы и коллекции - Словари - Значение по ключу

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,11 +2,17 @@ package core
 
 fun main() {
     val dic = mapOf(
-            1 to "one", 2 to "two"
+            1 to "one", 2 to null
     )
-    val keys = dic.keys
+    val one = dic[1]
 
-    println(keys)
-    //values is [1, 2]
+    val two = dic[2]
+
+    val three = dic[3]
+
+    println(one)
+    println(two)
+    println(three)
+
 }
 
