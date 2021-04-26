45804c9412ea07666c75b8a6a47bf55f1974a7d2
Rassell <miraqq55@gmail.com>
2021-04-26 20:43:00 +0200

    Перегрузка оператора - Операторы эквивалентности

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,24 +1,23 @@
 package core
 
 fun main() {
-    var p = Point(1, 1)
-    p++
-    //p.x is 2 and p.y is 2
-    println("(${p.x}, ${p.y})")
-    ++p
-    //p.x is 3 and p.y is 3
-    println("(${p.x}, ${p.y})")
-    p = -p
-    //p.x is -3 and p.y is -3
-    println("(${p.x}, ${p.y})")
+    val p1 = Point(1, 2)
+    val p2 = Point(2, 2)
+    val p3 = Point(1, 1)
+    val equal1 = p1 == p2
+    //equal is False
+    val equal2 = p1 == p3
+    //equal2 is False
+    val equal3 = p1 != p3
+    //equal3 is True
+    println(equal1)
+    println(equal2)
+    println(equal3)
+
 }
 
 class Point(var x: Int, var y: Int) {
-    operator fun inc(): Point {
-        return Point(++x, ++y)
-    }
-
-    operator fun unaryMinus(): Point {
-        return Point(-x, -y)
+    override fun equals(other: Any?): Boolean {
+        return (other is Point) && (x == other.x) && (y == other.y)
     }
 }
