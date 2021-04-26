f5e51997c41fc93534a314f41137f6c2c40581b6
Rassell <miraqq55@gmail.com>
2021-04-26 04:49:15 +0200

    Работа с простыми типами - Дата и время - Изменение даты

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,14 +1,31 @@
 package core
 
+import java.time.LocalDateTime
 import java.util.*
 
 
 fun main() {
-    val cal = Calendar.getInstance()
-    cal.set(1945, 5, 9, 0, 0, 0)
-    val victoryDate = cal.time
+    //example with LocalDate
+    val now = LocalDateTime.now()
+    val yesterday = now.minusDays(1)
+    val tomorrow = now.plusDays(1)
+    val nextMonth = now.plusMonths(1)
+    val nextYear = now.plusYears(1)
 
-    println(victoryDate)
+    //example with Date
+    val cal = Calendar.getInstance()
+    cal.add(Calendar.DAY_OF_YEAR, -1)
+    val yeasterday1 = cal.time
+
+    cal.add(Calendar.DATE, 1)
+    val tomorrow1 = cal.time
+
+    cal.add(Calendar.MONTH, 1)
+    val nextMonth1 = cal.time
+
+    cal.add(Calendar.YEAR, 1)
+    val nextYear1 = cal.time
+
 
 }
 
