e5e5e5a2994bdb6629423eb6592c07a1ee2cdb74
Rassell <miraqq55@gmail.com>
2021-04-26 19:32:42 +0200

    Перечесления Enum - Получение списка значений

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,20 +1,10 @@
 package core
 
 fun main() {
-    var rect = Rect(Point(3, 4), Size(10, 12))
-    //There is no with expression in Kotlin
-    rect.point.apply {
-        x += 1
-        y += 1
-    }
-
+    val values = Season.values()
+    values.forEach { println("${it}") }
 }
 
-class Point(var x: Int, var y: Int) {}
-
-class Size(var width: Int, var height: Int) {}
-
-class Rect(val point: Point, var size: Size) {}
-
-
-
+enum class Season {
+    Summer, Fall, Winter, Spring
+}
