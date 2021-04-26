1ab4d5f9797ee1d6c8dea7ac2d60ec681e21d57e
Rassell <miraqq55@gmail.com>
2021-04-27 01:38:29 +0200

    Массивы и коллекции - Словари - Преобразование словаря

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -4,15 +4,9 @@ fun main() {
     val dic = mapOf(
             1 to "one", 2 to null
     )
-    val one = dic[1]
+    val upperDic = dic
+            .mapValues { it.value?.toUpperCase() }
 
-    val two = dic[2]
-
-    val three = dic[3]
-
-    println(one)
-    println(two)
-    println(three)
-
+    println(upperDic)
 }
 
