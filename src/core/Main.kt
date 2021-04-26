560b6e8bf4602ab04918c70b9fdce28a79b7a6fc
Rassell <miraqq55@gmail.com>
2021-04-26 18:55:21 +0200

    Поток управления - Условные операторы - Оператор when - оператор "goto case"

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,20 +1,15 @@
 package core
 
 fun main() {
-    var str: String
-// val moniterInchSize = 24
-    when (24) {  //moniterInchSize
-        15 -> str = "too small"
-        in 16..18 ->
-            str = "good for the pas decade"
-        in 19..23 ->
-            str = "for office work"
-        in 24..27 ->
-            str = "great choice"
-        else ->
-            str = ""
-    }
-    println(str)
+    var sNumber = "1"
+    var numberList = ""
+
+    if (sNumber <= "1") numberList = "1"
+    if (sNumber <= "2") numberList += "-2"
+    if (sNumber <= "3") numberList += "-3"
+
+    println(numberList)
+
 }
 
 
