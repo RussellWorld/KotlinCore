38bf23f23fac638d21ba3cda136a0a72bb2feee3
Rassell <miraqq55@gmail.com>
2021-04-27 01:51:37 +0200

    Массивы и коллекции - Очередь Queue<T> (FIFO)

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,14 +1,20 @@
 package core
 
-fun main() {
-    val dic = mapOf(
-            3 to "B", 2 to "A", 1 to "C"
-    )
-    val oddDic = dic.toList()
-            .filter { (k, _) -> k % 2 == 1 }
-            .toMap()
-    //oddDic is {3=B, 1=C}
+import java.util.*
 
-    println(oddDic)
+fun main() {
+    val intQueue = LinkedList<Int>()
+    intQueue.add(1)
+    intQueue.add(3)
+    intQueue.add(5)
+
+    val top = intQueue.peek() //  top is 1
+
+    val first = intQueue.poll() // 1
+
+    val second = intQueue.poll() // 3
+
+    val thrid = intQueue.poll() // 5
+
 }
 
