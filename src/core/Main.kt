0b85d824ed5239f920d045b2e5bbd41e9aa529a4
Rassell <miraqq55@gmail.com>
2021-04-26 18:21:03 +0200

    Поток управления - Циклы - Бесконечные циклы

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,15 +1,15 @@
 package core
 
 fun main() {
-    var i = 7
-    var f7 = 1
+    while (true) {
+        //statements
+        var i = 0
+        i++
+        println(i * 2)
 
+    }
     do {
-        f7 *= i
-        i--
-    } while (i > 1)
-    //f7 is 5040
 
-    println(f7)
+    } while (true)
 }
 
