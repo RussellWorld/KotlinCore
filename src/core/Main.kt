3f6f80fab4bc2d235fffcf3956995eabdb465c75
Rassell <miraqq55@gmail.com>
2021-04-26 04:19:03 +0200

    Работа с простыми типами - Дата и время - получение года, месяца, дня, время

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,8 +1,17 @@
 package core
 
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 fun main() {
-  val now = LocalDateTime.now()
+    val now = LocalDate.now()
+    val time = LocalDateTime.now()
+    val year = now.year
+    val month = now.month
+    val day = now.dayOfMonth
+    val hour = time.hour
+    val minute = time.minute
+    val second = time.second
+    val dayOfWeek = now.dayOfWeek.value
 }
 
