e6f8c01a066b09bde70aeb7a3608466a70b95fdc
Rassell <miraqq55@gmail.com>
2021-04-27 01:48:26 +0200

    Массивы и коллекции - Словари - Фильтрация элементов

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -4,11 +4,11 @@ fun main() {
     val dic = mapOf(
             3 to "B", 2 to "A", 1 to "C"
     )
-    val sortedDic = dic.toList()
-            .sortedBy { (key, _) -> key }
+    val oddDic = dic.toList()
+            .filter { (k, _) -> k % 2 == 1 }
             .toMap()
-    //sortedDic is {1=C, 2=A, 3=B}
+    //oddDic is {3=B, 1=C}
 
-    println(sortedDic)
+    println(oddDic)
 }
 
