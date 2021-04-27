eb1b6e9c8ff92f2f610e617f116dd39d4666c0fe
Rassell <miraqq55@gmail.com>
2021-04-27 02:27:04 +0200

    Массивы и коллекции - Множества - Сортеровка элементов

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -3,9 +3,14 @@ package core
 import java.util.*
 
 fun main() {
-    val set = setOf(1, 2, 3)
-    val set3 = set.map { it * 3 }
-// 3, 6, 9
-    println(set3)
+    val chars = hashSetOf('B', 'A', 'C', 'D')
+    var str = ""
+    for (c in chars) {
+        if (str != "")
+            str += "; "
+        str += c
+    }
+    // A; B; C; D
+    println(str)
 }
 
