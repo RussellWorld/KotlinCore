bcba7d473202aa12f8f1091d084d2a83ecc83312
Rassell <miraqq55@gmail.com>
2021-04-27 03:07:10 +0200

    Массивы и коллекции - Массивы - Добавление и удаление элементов

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,9 +2,18 @@ package core
 
 
 fun main() {
-    val numbers = arrayOf(1, 2, 3)
-    val length = numbers.size
-    //length is 3
-    println(length)
+    var primeNumbers = mutableListOf(2, 5, 7)
+
+    primeNumbers.add(11)
+    //2,5,7,11
+    primeNumbers.add(1, 3)
+    //2,3,5,7,11
+    primeNumbers.removeAt(2)
+    //2,3,7,11
+    primeNumbers.addAll(arrayOf(13, 17))
+    //2,3,7,11,13,17
+    primeNumbers.clear()
+    //empty []
+
 }
 
