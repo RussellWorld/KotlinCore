2d060c45b8e270d58a19b53d1e4fe38a592ecb6f
Rassell <miraqq55@gmail.com>
2021-04-27 02:08:10 +0200

    Массивы и коллекции - Множества - HashSet and SortedSet

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -3,18 +3,20 @@ package core
 import java.util.*
 
 fun main() {
-    val intQueue = LinkedList<Int>()
-    intQueue.add(1)
-    intQueue.add(3)
-    intQueue.add(5)
+    val chars = hashSetOf('B', 'A', 'D', 'C')
+    println(chars)
+    //chars A, B, C, D
 
-    val top = intQueue.peek() //  top is 1
+    val sortedChars = sortedSetOf('B', 'H', 'D', 'A')
+    println(sortedChars)
+    //A, B, D, H
 
-    val first = intQueue.poll() // 1
-
-    val second = intQueue.poll() // 3
-
-    val thrid = intQueue.poll() // 5
+    val maxValue = sortedChars.max()
+    //H
+    println(maxValue)
+    val minValue = sortedChars.min()
+    //A
+    println(minValue)
 
 }
 
