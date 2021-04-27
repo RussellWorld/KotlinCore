eec6f52498277e3c31d8e8474b0a03603ad3bc73
Rassell <miraqq55@gmail.com>
2021-04-27 04:19:47 +0200

    Массивы и коллекции - Массивы - Проверка наличия элемента

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,10 +2,20 @@ package core
 
 
 fun main() {
-    val numbers = arrayOf(1, 2, 3, 4, 5)
-    val numbers2 = numbers.map { i -> i * 3 }
-    //3,6,9,12,15
-    numbers2.forEach { print("$it ") }
+    val numbers = arrayOf(2, 3, 5, 7, 11, 13, 17)
+    var contain5 = numbers.contains(5)
+    //True
+
+    val index5 = numbers.indexOf(5)
+    //is 2
+
+    val contain10 = numbers.contains(10)
+    //is False
+
+    println(contain5)
+    println(index5)
+    println(contain10)
+
 }
 
 
