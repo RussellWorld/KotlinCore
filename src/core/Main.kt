8e4796b28f9aab835b187b04b4aa9ee5798c1b6d
Rassell <miraqq55@gmail.com>
2021-04-27 02:24:25 +0200

    Массивы и коллекции - Множества - Преобразование множества

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -3,15 +3,9 @@ package core
 import java.util.*
 
 fun main() {
-    val chars = setOf('A', 'B', 'C', 'D')
-    val containA = chars.contains('A')
-    //True
-
-    val containE = chars.contains('E')
-    //False
-
-    val chars2 = setOf('A', 'B')
-    val containAll = chars.containsAll(chars2)
-    //True
+    val set = setOf(1, 2, 3)
+    val set3 = set.map { it * 3 }
+// 3, 6, 9
+    println(set3)
 }
 
