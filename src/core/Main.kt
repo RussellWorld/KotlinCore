269818f8e5751f929d23fcc7bd50441d238c2b81
Rassell <miraqq55@gmail.com>
2021-04-27 03:01:30 +0200

    Массивы и коллекции - Массивы - Динамические массивы

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,22 +1,9 @@
 package core
 
-import java.util.*
 
 fun main() {
-    val first = setOf(1, 2)
-    val second = setOf(1, 2)
-    val thrid = setOf(1, 2, 3)
-
-    val isEqual = first.equals(second)
-    //True
-
-    val isEqual1 = first == second
-    //True
-
-    val isIntersects = first.intersect(thrid).any()
-    //True
-
-    val isThirdSubsetOfFirst = first.containsAll(thrid)
-    //False
+    val count = 15
+    val arInt = Array(count, { 0 })
+    arInt[0] = 1
 }
 
