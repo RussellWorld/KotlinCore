33194d92bf2fed1c89df8aa08e1942d905b9ca6a
Rassell <miraqq55@gmail.com>
2021-04-27 03:28:44 +0200

    Массивы и коллекции - Массивы - Массив с заданным значением

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,8 +2,19 @@ package core
 
 
 fun main() {
-    val numbers1 = arrayOf(1, 2, 3, 4, 5)
-    val numbers2 = numbers1.copyOf()
+    val value = 5
+    val count = 3
 
+    //first method
+    val array1 = Array(count) { value }
+    //array is {5, 5, 5 }
+
+    //second method
+    val array2 = IntArray(count) { _ -> value }
+    //array is { 5, 5, 5}
+
+    array1.forEach { print("${it} ") }
+    println()
+    array2.forEach { print("$it ") }
 }
 
