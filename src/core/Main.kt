5e057b810906cb57c470e8566ffdbbcdf10c2f20
Rassell <miraqq55@gmail.com>
2021-04-27 04:00:22 +0200

    Массивы и коллекции - Массивы - Обход массива c индексом

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -4,11 +4,11 @@ package core
 fun main() {
     val numbers = arrayOf(2, 3, 4, 5, 6, 7, 8, 15, 18)
     var str = ""
-    for (number in numbers) {
+    for ((i, _) in numbers.withIndex()) {
         if (str.isNotEmpty()) {
             str += "; "
         }
-        str += number.toString()
+        str += numbers[i].toString()
     }
     // 2; 3; 4; 5; 6; 7; 8; 15; 18
     println(str)
