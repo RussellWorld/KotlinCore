b1270b9a7ebcbc81b9e493a1102e8566c263253d
Rassell <miraqq55@gmail.com>
2021-04-27 04:11:15 +0200

    Массивы и коллекции - Массивы - Получение части массива

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,10 +2,15 @@ package core
 
 
 fun main() {
-    val numbers = arrayOf(1, 3, 2, 1, 3)
-    val unique = numbers.distinct()
-    println(unique)
-    //1,3,2
+    val numbers = arrayOf(2, 3, 5, 7, 11)
+    val first2 = numbers.copyOf(2)
+    //2,3
+    val last3 = numbers.copyOfRange(2, 5)
+    //last3 is 5,7,11
+
+    first2.forEach { print("$it ") }
+    println()
+    last3.forEach { print("$it ") }
 }
 
 
