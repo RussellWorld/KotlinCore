e9068d4a5ffc2fdc4e6d97daf5edd07dcc934b5e
Rassell <miraqq55@gmail.com>
2021-04-27 03:14:36 +0200

    Массивы и коллекции - Массивы - Инициализация массивов

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,18 +2,24 @@ package core
 
 
 fun main() {
-    var primeNumbers = mutableListOf(2, 5, 7)
+    //Empty array
+    val n1 = IntArray(0)
+    val n2 = intArrayOf()
 
-    primeNumbers.add(11)
-    //2,5,7,11
-    primeNumbers.add(1, 3)
-    //2,3,5,7,11
-    primeNumbers.removeAt(2)
-    //2,3,7,11
-    primeNumbers.addAll(arrayOf(13, 17))
-    //2,3,7,11,13,17
-    primeNumbers.clear()
-    //empty []
+    //Singl-dimensional array
+    val n3 = arrayOf(1, 2, 3)
+    val n4 = intArrayOf(1, 2, 3)
+    val n5: IntArray = intArrayOf(1, 2, 3)
+    val s1 = arrayOf("1", "2", "3")
 
+    //Multidimensional array
+    val n6 = Array(2, { Array(2, { 0 }) })
+    val n7: Array<Array<Int>> = Array(2, { Array(3, { 0 }) })
+    n7[1][2] = 7
+
+    //Jagged array
+    val n8 = Array(2, { Array(2, { 0 }) })
+    n8[1] = Array(3, { 0 })
+    n8[1][2] = 7
 }
 
