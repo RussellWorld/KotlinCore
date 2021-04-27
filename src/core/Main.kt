5b8043451c0121349366883febc5b793bdfd69f5
Rassell <miraqq55@gmail.com>
2021-04-27 04:05:29 +0200

    Массивы и коллекции - Массивы - Объеденение массивов

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,14 +2,13 @@ package core
 
 
 fun main() {
-    val firstNumbers = arrayOf(2, 3, 5)
-    val secondNumbers = arrayOf(7, 11, 13)
-    val allNumbers = firstNumbers + secondNumbers
-    val allNumbers1 = secondNumbers + firstNumbers
-    //allNumbers 2,3,5,7,11,13
-
-    println(allNumbers)
-    println(allNumbers1)
+    val numbers = arrayOf(2, 3, 5, 7, 11)
+    val min = numbers.min()
+    //min is 2
+    val max = numbers.max()
+    //max is 11
+    println(min)
+    println(max)
 }
 
 
