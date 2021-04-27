51cea2ec40f0549a9a03235c078094185d177fa4
Rassell <miraqq55@gmail.com>
2021-04-27 03:56:50 +0200

    Массивы и коллекции - Массивы - Обход массива

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,36 +2,18 @@ package core
 
 
 fun main() {
-    val numbers = arrayOf(1, 2, 3, 4, 5)
-
-    val allLess10 = every(numbers, { x -> x < 10 })
-    //True
-
-    val someMore3 = some(numbers, { x -> x > 3 })
-    //True
-
-    val allOdd = every(numbers, { x -> x % 2 == 1 })
-    //False
-
-    println(allLess10)
-    println(someMore3)
-    println(allOdd)
-
-}
-
-fun <T> every(arr: Array<T>, f: (i: T) -> Boolean): Boolean {
-    for (v in arr) {
-        if (!f(v)) return false
+    val numbers = arrayOf(2, 3, 4, 5, 6, 7, 8, 15, 18)
+    var str = ""
+    for (number in numbers) {
+        if (str.isNotEmpty()) {
+            str += "; "
+        }
+        str += number.toString()
     }
-    return true
+    // 2; 3; 4; 5; 6; 7; 8; 15; 18
+    println(str)
 }
 
-fun <T> some(arr: Array<T>, f: (i: T) -> Boolean): Boolean {
-    for (v in arr) {
-        if (!f(v)) return true
-    }
-    return false
-}
 
 
 
