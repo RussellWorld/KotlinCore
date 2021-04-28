f101244c153609434f5e1fd25fd7c2ee663ca697
Rassell <miraqq55@gmail.com>
2021-04-28 23:28:38 +0200

    Интерфейсы - Требование к методам-индексаторам

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,19 +1,31 @@
 package core
 
+import java.lang.Math.pow
+import kotlin.math.pow
+
 
 fun main() {
-    val car = Airwave(1500, "Honda Airwave")
-    println(car.name)
+    val power = PowerOfTwo()
+    val p8 = power[8]
+    //is 256
+
+    val p16 = power[16]
+    //is65636
+
+    println(p8)
+    println(p16)
 }
 
-interface ICar {
-    val engineVolume: Int
-    val name: String
-
+interface IntIndex {
+    operator fun get(i: Int): Int
 }
 
-class Airwave(override var engineVolume: Int,
-        override val name: String) : ICar
+class PowerOfTwo : IntIndex {
+    override fun get(i: Int): Int {
+        return 2.0.pow(i.toDouble()).toInt()
+    }
+
+}
 
 
 
