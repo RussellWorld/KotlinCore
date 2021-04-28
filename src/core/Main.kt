39971dd3a8bcb806d3b5f84b2b334e8ebc26040f
Rassell <miraqq55@gmail.com>
2021-04-28 23:17:27 +0200

    Интерфейсы - Объявление и инициализация

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,12 +2,12 @@ package core
 
 
 fun main() {
-    val numbers = arrayOf(1, 2, 3, 4, 5)
-    var oddItems = numbers.filter { i -> i % 2 == 1 }
-    //oddItems 1, 3, 5
-    println(oddItems)
+    val printable = Printable() //<-- Error
 }
 
+interface Printable {
+    fun print()
+}
 
 
 
