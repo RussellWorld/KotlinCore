5f436a3c53b9710a0de26b850b8cfaa89a869402
Rassell <miraqq55@gmail.com>
2021-04-29 00:33:22 +0200

    Обобщенные типы - Принцип подстановки

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,26 +2,28 @@ package core
 
 
 fun main() {
-    val n1 = arrayOf(5)
-    val n2 = arrayOf(7)
-    swap(n1, n2)
-    //n1[0] is 7 and n2[0] is 5
+    val list = mutableListOf<Vehicle>()
+    val vehicle = Vehicle()
+    val car = Car()
+    val truck = Truck()
 
-    val s1 = arrayOf("cat")
-    val s2 = arrayOf("dog")
-    swap(s1, s2)
-    //s1[0] is dog / s2[0] is cat
+    list.add(vehicle)
+    list.add(car)
+    list.add(truck)
 
-    println("n1[0] is ${n1[0]} and n2[0] is ${n2[0]}")
-    println("s1[0] is ${s1[0]} and s2[0] is ${s2[0]}")
-
-
-}
-
-fun <T> swap(a: Array<T>, b: Array<T>) {
-    val tmp = a[0]
-    a[0] = b[0]
-    b[0] = tmp
+    for (curVehicle in list) {
+        curVehicle.test()
+    }
 
 }
 
+open class Vehicle {
+    fun test() {
+
+    }
+
+}
+
+class Car : Vehicle()
+
+class Truck : Vehicle()
