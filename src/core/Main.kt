df8cc0577fe068a863cba703d9803081e9548172
Rassell <miraqq55@gmail.com>
2021-04-29 21:21:59 +0200

    Работа с файлами - Базовые операции - Перемещение файла

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,14 +2,17 @@ package core
 
 import java.io.*
 import java.lang.Exception
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 
 
 fun main() {
-    val dir = File("data")
-    val dirCopy = File("data_copy")
+    val file = Paths.get("file.txt")
+    val toFile = Paths.get("new_file.txt")
     try {
-        dir.copyRecursively(dirCopy, true)
-        println("successfully copied")
+        Files.move(file, toFile, StandardCopyOption.REPLACE_EXISTING)
+        println("file moved")
     } catch (e: Exception) {
         println(e.message)
     }
