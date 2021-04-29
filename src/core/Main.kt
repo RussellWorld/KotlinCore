917c6664663973f42b8b3e46bc7dc6738f418f4a
Rassell <miraqq55@gmail.com>
2021-04-30 00:07:41 +0200

    Регулярные выражения - Замена совпадений

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,12 +2,11 @@ package core
 
 
 fun main() {
-    var data = "x77x6Fx72x6Cx64"
-    val regex = "x([0-9A-F]{2})".toRegex()
-    data = regex.replace(data) { result ->
-        val b = result.groups[1]!!.value.toInt(16)
-        b.toChar().toString()
-    }
-    //data is "world"
+    var data = "Pi = 3.14, exponent = 2.718"
+    val regex = """\d+\.\d+""".toRegex()
+    data = regex.replace(data, "<f>$0</f>")
+
+    //data is "Pi = <f>3.14</f>, exponent = <f>2.718</f>"
+
     println(data)
 }
