8129d61d331738f312e1f8ca2d9058a35dd91a50
Rassell <miraqq55@gmail.com>
2021-04-29 21:07:04 +0200

    Работа с файлами - Текстовые файлы - Чтение из файла

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,32 +1,19 @@
 package core
 
 import java.io.File
-import java.io.FileWriter
 import java.io.IOException
-import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardOpenOption
 
 
 fun main() {
     try {
-        //first method
-        File("file1.txt").printWriter().use { out ->
-            out.println("Line 1")
-            out.println("Line 2")
-        }
+        val text = File("file1.txt")
+                .readText()
+        println(text)
 
-        //second method
-        val lines = mutableListOf("Line 1", "Line 2")
-        val file = Paths.get("file2.txt")
-        Files.write(file, lines, Charset.defaultCharset())
-
-        //thrid method
-        File("file3.txt").writeText("Line 1\nLine 2")
     } catch (e: IOException) {
         println(e.message)
     }
+
 }
 
 
