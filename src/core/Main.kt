51a68c2abab44a27bba352655148ed0668b4d6ba
Rassell <miraqq55@gmail.com>
2021-04-29 20:34:30 +0200

    Лямбда выражения - Без возвращаемого значения

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,28 +2,9 @@ package core
 
 
 fun main() {
-    val payFromWallet1 = makeWallet(arrayOf(1000))
-    val payFromWallet2 = makeWallet(arrayOf(500))
-    var balance = payFromWallet1(50)
-    //balance is 950
-    println(balance)
-
-    balance = payFromWallet2(70)
-    //balance is 430
-
-    println(balance)
-
-    balance = payFromWallet1(150)
-    //balance is 800
-
-    println(balance)
-}
-
-fun makeWallet(sum: Array<Int>): (Int) -> Int {
-    return { pay: Int ->
-        sum[0] -= pay
-        sum[0]
-    }
+    val add2AndPrint = { a: Int -> print(a + 2) }
+    add2AndPrint(5)
+    // is 7
 }
 
 
