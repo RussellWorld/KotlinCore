1f8d7cb4adccc57ede27067bf6a866bc17ebb05a
Rassell <miraqq55@gmail.com>
2021-04-30 01:01:27 +0200

    Многопоточные операции - Асинхронный вызов

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,15 +2,12 @@ package core
 
 
 fun main() {
-    val data1 = "aaab"
-    val data2 = "aaaba"
-    val regex = "a+b".toRegex()
-    val b1 = regex.matches(data1)
-    //b1 is true
-
-    val b2 = regex.matches(data2)
-    //b2 is false
-
-    println(b1)
-    println(b2)
+    val i = 5
+    val action = {
+        Thread.sleep(3_000)
+        println(i * 10)
+    }
+    //Run lambda at new thread
+    val thread = Thread(action)
+    thread.start()
 }
