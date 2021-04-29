7796a67740ddf43456155a238b485de74c90364e
Rassell <miraqq55@gmail.com>
2021-04-29 21:37:50 +0200

    Работа с файлами - Базовые операции - Получить рабочую директорию

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,12 +1,32 @@
 package core
 
+import jdk.nashorn.internal.runtime.Context.getContext
 import java.io.*
+import java.nio.file.Paths
+import java.util.*
 
 
 fun main() {
-    val file = File("file.txt")
-    val fileCopy = File("file_copyl.txt")
-    file.copyTo(fileCopy, true)
+    //on a desktop system
+    //get current file path (first method)
+    val path1 = System.getProperty("user.dir")
+    println("Working Directory = $path1")
+
+    //get current file path (second method)
+    val path2 = Paths.get("").toAbsolutePath().toString()
+    println("Working Directory = $path2")
+
+    //get document path
+    val fr = javax.swing.JFileChooser()
+    val fw = fr.fileSystemView
+    val documentsPath = fw.defaultDirectory.toString()
+    println(documentsPath)
+
+    //on android activity
+    val path = this.getAplicationInfo().dataDir
+
+    //on android fragment
+    val path4 = getContext().getApplicationInfo().dataDir
 }
 
 
