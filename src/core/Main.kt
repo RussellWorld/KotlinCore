f63e42fd81e235849faf9a2787d2c734e7fbe925
Rassell <miraqq55@gmail.com>
2021-04-29 23:35:58 +0200

    Работа с базами данных (БД) - Подключение к БД - Подключение к SQlite

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,27 +1,32 @@
 package core
 
-import com.sun.org.apache.xalan.internal.xsltc.runtime.Node
-import org.redundent.kotlin.xml.*
-import java.io.*
-
-
-//// dependencies{
-//...
-// implementation 'org.redundent: kotlin-xml-builder:1.5.0'
-//}
 
 fun main() {
-    val file = File("data.xml")
-    val doc = parse(file)
-    for (node in doc.children) {
-        if (node is Node) {
-            val value = (node.children[0] as TextElement).text
-            val id = node.attributes["Id"].toString()
-            println("value: $valuem id: $id")
-        }
-    }
+    //Connect to the database
+    val dbHelper = DataBaseHelper(
+            context,
+            dbPath + "DatabaseName.sqlite"
+    )
 
+    //SQL query execution
+    val db = dbHelper.readableDatabase
+    val cursor = db.rawQuery(
+            "SELECT * FROM country", null)
+    )
+
+    //Get the data
+    cursor.moveToFirst()
+    val countryName = cursor.getString(0)
+
+    println(countryName)
+
+    //Close the connection
+    cursor.close()
 }
 
+class DataBaseHelper(context: Context, dbName: String) :
+        SQLiteOpenHelper(context, dbName, null, 1) {
+    override fun onCreate(db: SQLiteDataBase?) {}
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
 
-
+}
