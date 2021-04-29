6f153ce840061c850ffbfe8607f20ef8c5419689
Rassell <miraqq55@gmail.com>
2021-04-30 00:44:37 +0200

    Регулярные выражения - Проверка совпадений всей строки

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,12 +2,15 @@ package core
 
 
 fun main() {
-    val data = "Pi is equal to 3.14"
-    val regex = "\\d+\\.\\d+".toRegex()
-    val match = regex.find(data, 0)
-    if (match != null) {
-        var pi = match.value.toDouble()
-        //pi is 3.14
-        println(pi)
-    }
+    val data1 = "aaab"
+    val data2 = "aaaba"
+    val regex = "a+b".toRegex()
+    val b1 = regex.matches(data1)
+    //b1 is true
+
+    val b2 = regex.matches(data2)
+    //b2 is false
+
+    println(b1)
+    println(b2)
 }
