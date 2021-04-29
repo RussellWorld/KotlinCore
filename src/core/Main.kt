8d9b3af277b39b2abbcb461a43cc73513a2e3e77
Rassell <miraqq55@gmail.com>
2021-04-29 19:55:13 +0200

    Лямбда выражения - C несколькими операторами

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,20 +1,22 @@
 package core
 
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun main() {
-    //explicitly specify return type
-    val powOfTwo = { power: Int ->
-        2.0.pow(power.toDouble())
+    val getDistance = { p1: Point, p2: Point ->
+        val d1 = 2.0.pow(p1.x - p2.x)
+        val d2 = 2.0.pow(p1.y - p2.y)
+        sqrt(d1 + d2)
     }
-    val powe8 = powOfTwo(8)
-    //pow is 256.0
-
-    //implicitly specified return type
-    val powOfThree: (Double) -> Double = { power ->
-        3.0.pow(power)
-    }
-    val pow3 = powOfThree(3.0)
-    //pow3 is 27.0
+    val point = Point(0.0, 0.0)
+    val point2 = Point(5.0, 5.0)
+    val distance = getDistance(point, point2)
+    //distance is 7.071
+    println(distance)
 }
+
+class Point(var x: Double, var y: Double)
+
+
 
