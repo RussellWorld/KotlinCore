b6a417039f1fcfdfab48fe3b82244870a02ef4a3
Rassell <miraqq55@gmail.com>
2021-04-29 20:57:10 +0200

    Работа с файлами - Текстовые файлы - добавление текста в файл

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,14 +1,32 @@
 package core
 
+import java.io.FileWriter
+import java.io.IOException
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+
+
 
 fun main() {
-    val avgFunc = { a: Int, b: Int ->
-        (a + b) / 2
-    }
-    val avg = avgFunc(3, 5)
-    //avg is 4
+    try {
+        //fist method
+        val fw = FileWriter("file1.txt", true)
+        fw.write("Line 3\nLine 4")
+        fw.close()
 
-    println(avg)
+        //second method
+        val lines = mutableListOf("Line 3", "Line 4")
+        val file2 = Paths.get("file2.txt")
+        Files.write(file2, lines, Charset.defaultCharset(), StandardOpenOption.APPEND)
+
+        //thrid method
+        val file3 = Paths.get("file3.txt")
+        Files.write(file3, "\n Line 3 \n Line 4".toByteArray(), StandardOpenOption.APPEND)
+    } catch (e: IOException) {
+        println(e.message)
+    }
 }
 
 
