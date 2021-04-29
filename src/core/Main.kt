7d2425e9daa3c31e0ad36c6d07b45fa14a3f7426
Rassell <miraqq55@gmail.com>
2021-04-30 01:26:15 +0200

    Многопоточные операции - Синхронизация с блокировкой

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,13 +1,22 @@
 package core
 
+import java.util.concurrent.Executors
+
 
 fun main() {
-    val thread = MyThread()
-    thread.start()
+    val threahPool = Executors.newFixedThreadPool(10)
+
+    //start ten threads
+    for (i in 1..10) {
+        val action = Runnable {
+            showNumber(i)
+        }
+        threahPool.execute(action)
+    }
+    threahPool.shutdown()
 }
 
-class MyThread : Thread() {
-    override fun run() {
-        println("in my tread")
-    }
+fun showNumber(number: Int) = synchronized(lock = Any()) {
+    Thread.sleep((2500..5500).random().toLong())
+    print("$number, ")
 }
