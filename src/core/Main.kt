2b5b9eb2720c51d20351d55441de035c57d23d15
Rassell <miraqq55@gmail.com>
2021-04-29 21:23:18 +0200

    Работа с файлами - Базовые операции - Копирование файла

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,21 +1,12 @@
 package core
 
 import java.io.*
-import java.lang.Exception
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
 
 
 fun main() {
-    val file = Paths.get("file.txt")
-    val toFile = Paths.get("new_file.txt")
-    try {
-        Files.move(file, toFile, StandardCopyOption.REPLACE_EXISTING)
-        println("file moved")
-    } catch (e: Exception) {
-        println(e.message)
-    }
+    val file = File("file.txt")
+    val fileCopy = File("file_copyl.txt")
+    file.copyTo(fileCopy, true)
 }
 
 
