267d946ff1e79464d14d9e70f80dcc7fe0afaf17
Rassell <miraqq55@gmail.com>
2021-04-30 00:03:57 +0200

    Регулярные выражения - Замена с использованием функции

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,33 +2,12 @@ package core
 
 
 fun main() {
-    //Connect to the database
-    val dbHelper = DataBaseHelper(
-            context,
-            dbPath + "DatabaseName.sqlite"
-    )
-
-    //SQL query execution
-    val db = dbHelper.readableDatabase
-    val cursor = db.rawQuery(
-            """SELECT Language, Percentage FROM countryLanguage
-                | WHERE CountryCode = ? AND Percentage > ?""".trimMargin(), arrayOf("RUS","0,5"))
-    )
-
-
-    //Get All rows
-    while (!cursor.isAfterLast()) {
-        println("${cursor.getString(0)} : ${cursor.getDouble(1)}")
-        cursor.moveToNext()
+    var data = "x77x6Fx72x6Cx64"
+    val regex = "x([0-9A-F]{2})".toRegex()
+    data = regex.replace(data) { result ->
+        val b = result.groups[1]!!.value.toInt(16)
+        b.toChar().toString()
     }
-
-    //Close the connection
-    cursor.close()
-}
-
-class DataBaseHelper(context: Context, dbName: String) :
-        SQLiteOpenHelper(context, dbName, null, 1) {
-    override fun onCreate(db: SQLiteDataBase?) {}
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
-
+    //data is "world"
+    println(data)
 }
