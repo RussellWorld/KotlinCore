c650b46714d447ceaae186c2755a0639469686d6
Rassell <miraqq55@gmail.com>
2021-04-29 22:10:58 +0200

    Работа с файлами - Базовые операции - Список файлов в директории

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,17 +1,26 @@
 package core
 
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Node
+import org.redundent.kotlin.xml.*
 import java.io.*
-import java.lang.Exception
 
+
+//// dependencies{
+//...
+// implementation 'org.redundent: kotlin-xml-builder:1.5.0'
+//}
 
 fun main() {
-    val file = File("file.txt")
-    try {
-        file.delete()
-        println("file deleted")
-    }catch (e: Exception){
-        println(e.message)
+    val file = File("data.xml")
+    val doc = parse(file)
+    for (node in doc.children) {
+        if (node is Node) {
+            val value = (node.children[0] as TextElement).text
+            val id = node.attributes["Id"].toString()
+            println("value: $valuem id: $id")
+        }
     }
+
 }
 
 
