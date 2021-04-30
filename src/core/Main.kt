1b4f2eb6a9e92995639b0b568e8156cce9b011a8
Rassell <miraqq55@gmail.com>
2021-04-30 23:06:05 +0200

    Паттерны - Порождающие паттерны - Прототип

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,68 +1,32 @@
 package core
 
 fun main() {
-    //Client
-    val textMaker = TextMaker()
 
-    val textBuilder = TextBuilder()
-    textMaker.makeText(textBuilder)
-    val text = textBuilder.getText()
-    //text: Line1 Line2
+    val square = Square()
+    val maker = ShapeMaker(square)
 
-    val htmlBuilder = HtmlBuilder()
-    textMaker.makeText(htmlBuilder)
-    val html = htmlBuilder.getHtml()
-    //html: <span> line 1</span><br/> <span> line 2 </span>
+    val square1 = maker.makeShape()
+    val square2 = maker.makeShape() as Square
 
-    println(text)
-    println(html)
+    println(square1)
+    println(square2.lineCount)
 }
 
-//AbstractBuilder
-interface ITextBuilder {
-    fun addText(value: String)
-    fun addNewLine(value: String)
+//Prototype
+interface Shape {
+    fun clone(): Shape
 }
 
-//ConcreteBuilder 1
-class TextBuilder : ITextBuilder {
-    private var text = ""
-
-    override fun addText(value: String) {
-        text += value
-    }
-
-    override fun addNewLine(value: String) {
-        text += "\n" + value
-    }
-
-    fun getText(): String {
-        return text
+//ConcretePrototype
+class Square(var lineCount: Int = 4) : Shape {
+    override fun clone(): Shape {
+        return Square(lineCount)
     }
 }
 
-//ConcreteBuilder 2
-class HtmlBuilder : ITextBuilder {
-    private var html = ""
-    override fun addText(value: String) {
-        html += "<span>$value</span>"
-    }
-
-    override fun addNewLine(value: String) {
-        html += "br/>\n"
-        addText(value)
-    }
-
-    fun getHtml(): String {
-        return html
+//Client
+class ShapeMaker(var shape: Shape) {
+    fun makeShape(): Shape {
+        return shape.clone()
     }
 }
-
-//Director
-class TextMaker {
-    fun makeText(textBuilder: ITextBuilder) {
-        textBuilder.addText("Line 1")
-        textBuilder.addNewLine("Line 1")
-    }
-}
-
