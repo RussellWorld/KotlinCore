8f2cb22a93f0fa812066034c75eb35eb08daf306
Rassell <miraqq55@gmail.com>
2021-04-30 22:33:31 +0200

    Паттерны - Порождающие паттерны -> Абстрактная фабрика

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,44 +1,83 @@
 package core
 
 fun main() {
-    //Client
-    var creator = Creator()
-    creator.test()
-
-    creator = ManagerCreator()
-    creator.test()
+    testFactory(Factory1())
+    //printed: test A1 and test B1
+    testFactory(Factory2())
+    //printed: test A2 and test B2
 }
 
-//Product
-open class Employee {
-    open fun test() {
-        println("Employee")
-    }
+//client code
+fun testFactory(factory: IFactory) {
+    val productA = factory.createA()
+    val productB = factory.createB()
+    productA.testA()
+    productB.testB()
 }
 
-//ConcreteProduct
-class Manager : Employee() {
-    override fun test() {
-        println("Manager")
-    }
-}
-
-//Creator
-open class Creator {
-    //FactoryMethod
-    open fun createEmployee(): Employee {
-        return Employee()
-    }
-
-    //Some operation
-    fun test() {
-        createEmployee().test()
+//concrete product A1
+class ProductA1 : IProductA {
+    override fun testA() {
+        println("test A1")
     }
 }
 
-//ConcreteCreator
-class ManagerCreator : Creator() {
-    override fun createEmployee(): Employee {
-        return Manager()
+//concrete product A2
+class ProductA2 : IProductA {
+    override fun testA() {
+        println("test A2")
     }
+}
+
+//concrete product B1
+class ProductB1 : IProductB {
+    override fun testB() {
+        println("test B1")
+    }
+}
+
+//concrete product B2
+class ProductB2 : IProductB {
+    override fun testB() {
+        println("test B2")
+    }
+}
+
+//concrete factory 1
+class Factory1 : IFactory {
+    override fun createA(): IProductA {
+        return ProductA1()
+    }
+
+    override fun createB(): IProductB {
+        return ProductB1()
+    }
+}
+
+//concrete factory 2
+class Factory2 : IFactory {
+    override fun createA(): IProductA {
+        return ProductA2()
+    }
+
+    override fun createB(): IProductB {
+        return ProductB2()
+    }
+}
+
+//abstract factory
+interface IFactory {
+    fun createA(): IProductA
+    fun createB(): IProductB
+}
+
+
+//abstact product B
+interface IProductB {
+    fun testB()
+}
+
+//Abstract product A
+interface IProductA {
+    fun testA()
 }
