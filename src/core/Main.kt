e0ea54ccf7cc26604ccbfcbfcfdde8a1c68fc657
Rassell <miraqq55@gmail.com>
2021-04-30 22:12:44 +0200

    Паттерны - Порождающие паттерны -> Фабричный метод

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,27 +1,44 @@
 package core
 
+fun main() {
+    //Client
+    var creator = Creator()
+    creator.test()
 
-//on Android apk
+    creator = ManagerCreator()
+    creator.test()
+}
 
-class MainActivity : AppCompatActivity() {
-    fun onClick() {
-        CalcTask().execute
+//Product
+open class Employee {
+    open fun test() {
+        println("Employee")
+    }
+}
+
+//ConcreteProduct
+class Manager : Employee() {
+    override fun test() {
+        println("Manager")
+    }
+}
+
+//Creator
+open class Creator {
+    //FactoryMethod
+    open fun createEmployee(): Employee {
+        return Employee()
     }
 
-    fun add(a: Int, b: Int): Int {
-        Thread.sleep(3000)
-        return a + b
+    //Some operation
+    fun test() {
+        createEmployee().test()
     }
+}
 
-    private inner class CalcTask : AsyncTask<Unit, Unit, Int>() {
-        override fun doInBackground(vararg params: Unit): Int {
-            return add(3, 5)
-        }
-
-        override fun onPostExecute(result: Int?) {
-            //synchronization with the main thread
-            //for update user interface
-            mResultTextView.setText(result!!.toString())
-        }
+//ConcreteCreator
+class ManagerCreator : Creator() {
+    override fun createEmployee(): Employee {
+        return Manager()
     }
 }
