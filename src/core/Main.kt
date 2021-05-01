f19bae1a714419ae9ac02622a293a9a62e7d2073
Rassell <miraqq55@gmail.com>
2021-05-01 03:35:32 +0200

    Паттерны - Паттерны поведения - Шаблонный метод

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -2,52 +2,37 @@ package core
 
 
 fun main() {
-    //Client
-    val adapter = getTextAdapter()
-    val text = adapter.getText()
-    //text: line 1 and line 2
-
-    println(text)
+//Client
+    val circle = Circle()
+    circle.draw()
 }
 
-//Target
-interface IText {
-    //Request()
-    fun getText(): String
-}
-
-//Adaptee
-open class StringList {
-    private val rows = mutableListOf<String>()
-
-    //SpecificRequest()
-    fun getString(): String {
-        return rows.joinToString("\n")
-    }
-
-    fun add(value: String) {
-        rows.add(value)
-    }
-}
-
-//Adapter
-class TextAdapter : IText {
-    var rowList: StringList? = null
-
-    override fun getText(): String {
-        if (rowList == null) {
-            return ""
+abstract class Shape {
+    //Template method
+    fun draw() {
+        if (!canDraw()) {
+            return
         }
-        return rowList!!.getString()
+        doDraw()
+        notifyListeners()
+    }
+
+    fun canDraw(): Boolean {
+        //if it possiable to draw the shape
+        return true
+    }
+
+    //primitive operation
+    protected abstract fun doDraw()
+
+    fun notifyListeners() {
+        println("shape is drawn")
     }
 }
 
-fun getTextAdapter(): TextAdapter {
+class Circle : Shape() {
+    override fun doDraw() {
+        println("draw a circle")
+    }
 
-    val adapter = TextAdapter()
-    val rowList = StringList()
-    rowList.add("line 1")
-    rowList.add("line 2")
-    adapter.rowList = rowList
-    return adapter
 }
