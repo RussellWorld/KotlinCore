997b48414717f774eb35baf0557f8c2f56d8244c
Rassell <miraqq55@gmail.com>
2021-05-03 23:13:07 +0200

    Алгоритмы - Сортировака - Сортеровка подсчетом (Глупая сортировка)

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -1,84 +1,66 @@
 package core
 
+import java.util.*
+
+
+// Time Complexity O(n+k)
+// Space Complexity O(k)
 
 fun main() {
-    val car = Car()
-    val v1 = TestCarVisitor()
-    val v2 = RepairCarVisitor()
+    var items = arrayOf(4, 1, 5, 3, 2, 6, 7)
 
-    car.accept(v1)
-    car.accept(v2)
+    val sortItems = sort(items)
+// sortItems is {1, 2, 3, 4, 5}
+    sortItems.forEach { print("$it ") }
+    println()
+
+//// *** simplified speed test ***
+    items = Array(200) { 0 }
+            .mapIndexed { i, _ -> i }
+            .toTypedArray()
+    val tmp = items[5]
+    items[5] = items[6]
+    items[6] = tmp
+    val count = 10000
+    val start = Date()
+
+    for (i in 0 until count) {
+        sort(items)
+    }
+
+    val milliseconds = Date().time - start.time
+
+    println(milliseconds)
+//// about 788 milliseconds
 }
 
-//Visitor
-interface CarVisitor {
-    fun visit(engine: Engine)
-    fun visit(wheel: Wheel)
-    fun visit(car: Car)
+fun sort(arr: Array<Int>): Array<Int> {
+    val items = Array(arr.size) { 0 }
+
+    var min = Int.MAX_VALUE
+    var max = Int.MIN_VALUE
+    for (x in arr) {
+        if (x > max) max = x
+        if (x < min) min = x
+    }
+
+    val counts = Array(max - min + 1) { 0 }
+
+    for (x in arr) {
+        counts[x - min]++
+    }
+
+    var total = 0
+    for (i in min..max) {
+        val oldCount = counts[i - min]
+        counts[i - min] = total
+        total += oldCount
+    }
+
+    for (x in arr) {
+        items[counts[x - min]] = x
+        counts[x - min]++
+    }
+    return items
 }
 
-//ConcreteVisitor
-class TestCarVisitor : CarVisitor {
-    override fun visit(engine: Engine) {
-        println("test engine")
-    }
-
-    override fun visit(wheel: Wheel) {
-        println("test wheel #${wheel.getNumber()}")
-    }
-
-    override fun visit(car: Car) {
-        println("test car")
-    }
-}
-
-//ConcreteVisitor
-class RepairCarVisitor : CarVisitor {
-    override fun visit(engine: Engine) {
-        println("repair engine")
-    }
-
-    override fun visit(wheel: Wheel) {
-        println("repair wheel #${wheel.getNumber()}")
-    }
-
-    override fun visit(car: Car) {
-        println("repair car")
-    }
-
-}
-
-interface Element {
-    fun accept(v: CarVisitor)
-}
-
-//ConcreteElement
-class Engine : Element {
-    override fun accept(v: CarVisitor) {
-        v.visit(this)
-    }
-}
-
-//ConcreteElement
-class Wheel(private val number: Int) : Element {
-    fun getNumber(): Int {
-        return number
-    }
-
-    override fun accept(v: CarVisitor) {
-        v.visit(this)
-    }
-}
-
-//ConcreteElement
-class Car : Element {
-    private val items = arrayOf(Engine(), Wheel(1), Wheel(2),
-            Wheel(3), Wheel(4))
-
-    override fun accept(v: CarVisitor) {
-        for (e in items) {
-            e.accept(v)
-        }
-        v.visit(this)
-    }
-}
