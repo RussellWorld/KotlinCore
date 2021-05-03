a8ee436999449ceed0c6198b59fe76963739e56f
Rassell <miraqq55@gmail.com>
2021-05-03 23:23:23 +0200

    Алгоритмы - Сортировака - Сортировка пузырьком

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -3,11 +3,11 @@ package core
 import java.util.*
 
 
-// Time Complexity O(n+k)
-// Space Complexity O(k)
+// Time Complexity O(n^2)
+// Space Complexity O(1)
 
 fun main() {
-    var items = arrayOf(4, 1, 5, 3, 2, 6, 7)
+    var items = arrayOf(4, 17, 15, 3, 2, 6, 7, 9, 11, 8, 14, 5, 16, 1)
 
     val sortItems = sort(items)
 // sortItems is {1, 2, 3, 4, 5}
@@ -31,35 +31,19 @@ fun main() {
     val milliseconds = Date().time - start.time
 
     println(milliseconds)
-//// about 788 milliseconds
+//// about 2788 milliseconds
 }
 
 fun sort(arr: Array<Int>): Array<Int> {
-    val items = Array(arr.size) { 0 }
-
-    var min = Int.MAX_VALUE
-    var max = Int.MIN_VALUE
-    for (x in arr) {
-        if (x > max) max = x
-        if (x < min) min = x
-    }
-
-    val counts = Array(max - min + 1) { 0 }
-
-    for (x in arr) {
-        counts[x - min]++
-    }
-
-    var total = 0
-    for (i in min..max) {
-        val oldCount = counts[i - min]
-        counts[i - min] = total
-        total += oldCount
-    }
-
-    for (x in arr) {
-        items[counts[x - min]] = x
-        counts[x - min]++
+    var items = arr.copyOf()
+    for (i in 0..items.size - 1) {
+        for (j in i + 1..items.size - 1) {
+            if (items[j] < items[i]) {
+                val tmp = items[j]
+                items[j] = items[i]
+                items[i] = tmp
+            }
+        }
     }
     return items
 }
