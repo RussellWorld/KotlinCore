a70c94cef4d90393914f0534f1f6907473d2f822
Rassell <miraqq55@gmail.com>
2021-05-04 03:28:31 +0200

    Алгоритмы - Поиск - Быстрый линейный поиск

--- src/core/Main.kt
+++ src/core/Main.kt
@@ -4,18 +4,18 @@ import java.util.*
 
 
 fun main() {
-    var items = listOf(2, 3, 5, 7, 11, 13, 17, 22, 25, 29, 32)
+    var items = mutableListOf(2, 3, 5, 7, 11, 13, 17, 22, 25, 29, 32)
 
     println(search(items, 1))
     //print -1
-    println(search(items, 7))
+    println(search(items, 32))
     //print 3
     println(search(items, 19))
     //print 3
 
 
     //   speed test
-    items = Array(1000000, { 0 }).mapIndexed { i, _ -> i }
+    items = Array(1000000, { 0 }).mapIndexed { i, _ -> i }.toMutableList()
     val count = 100
 
     val start = Date()
@@ -27,23 +27,21 @@ fun main() {
     val milliseconds = Date().time - start.time
     println(milliseconds)
 
-    //about 2
+    //about 471
 }
 
-fun search(list: List<Int>, x: Long): Int {
-    var low = 0
-    var high = list.size - 1
-
-    while ((list[low] < x) && (x < list[high])) {
-        var mid = (low + ((x - list[low]) * (high - low)) / (list[high] - list[low])).toInt()
-
-        if (list[mid] < x) low = mid + 1
-        else if (list[mid] > x) high = mid - -1
-        else return mid
+fun search(list: MutableList<Int>, x: Int): Int {
+    var i = 0
+    val count = list.size
+    list.add(x)
+    while (true) {
+        if (list[i] == x) {
+            if (i < count) {
+                return i
+            }
+            return -1
+        }
+        i++
     }
-    if (list[low] == x.toInt())
-        return low
-    if (list[high] == x.toInt())
-        return high
-    return -1
+    list.removeAt(count)
 }
